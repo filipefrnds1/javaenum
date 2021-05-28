01a4b8ae0fc555a608aeb18810622a94a494e717
Filipe Fernandes <filipefirmino50@gmail.com>
2021-05-27 22:25:22 -0300

    Transformando Tipos Enum em Strings pelo valueof

--- src/applicationenum/ProgramEnum.java
+++ src/applicationenum/ProgramEnum.java
@@ -18,6 +18,12 @@ public class ProgramEnum {
 		
 		System.out.println(order);
 		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 		
 		
 		sc.close();
